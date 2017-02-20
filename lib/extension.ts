8f6cc982a17987de93c35a2f3801e745e91ed1ab
Vlad Dumitrescu <vladdu55@gmail.com>
2017-02-20 16:37:46 +0100

    small cosmetic changes

--- lib/extension.ts
+++ lib/extension.ts
@@ -2,13 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 //import * as fs from 'fs'
-import * as rebar from './RebarRunner';
-//import * as erlang from './ErlangShell';
-import * as adapter from './vscodeAdapter';
 //import * as path from 'path';
-import * as eunitrunner from './eunitRunner';
-import * as utils from './utils';
-//import * as erlangDebug from './erlangDebug';
+
+import * as Adapter from './vscodeAdapter';
+import * as Rebar from './RebarRunner';
+import * as Eunit from './eunitRunner';
+import * as Utils from './utils';
+//import * as ErlShell from './ErlangShell';
+//import * as ErlDebug from './erlangDebug';
 
 var myoutputChannel : vscode.OutputChannel;
 var myConsole : vscode.OutputChannel;
@@ -16,12 +17,13 @@ var myConsole : vscode.OutputChannel;
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	myoutputChannel = adapter.ErlangOutput();
-	myConsole = utils.pgoConsole();
+	myoutputChannel = Adapter.ErlangOutput();
+	myConsole = Utils.pgoConsole();
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "erlang" is now active!'); 
-	myConsole.appendLine("erlang extension is acitve");
+	myConsole.appendLine("erlang extension is active");
+
 	//configuration of erlang language -> documentation : https://code.visualstudio.com/Docs/extensionAPI/vscode-api#LanguageConfiguration
 	var disposables=[];
 	// The command has been defined in the package.json file
@@ -29,10 +31,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	//disposables.push(vscode.commands.registerCommand('extension.rebarBuild', () => { runRebarCommand(['compile']);}));
 
-	var runner = new rebar.RebarRunner();
-	runner.activate(context.subscriptions);
+	var rebar = new Rebar.RebarRunner();
+	rebar.activate(context.subscriptions);
 
-	var erunner = new eunitrunner.EunitRunner();
-	erunner.activate(context);
+	var eunit = new Eunit.EunitRunner();
+	eunit.activate(context);
+
 	disposables.forEach((disposable => context.subscriptions.push(disposable)));
 }
