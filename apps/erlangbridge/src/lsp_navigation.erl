9adb0066afc38d46f250c70c29411c2b268e9afe
Wojtek Surowka <wojteksurowka@me.com>
2018-05-01 20:20:53 +0100

    Go To Definition for funs and variables

--- apps/erlangbridge/src/lsp_navigation.erl
+++ apps/erlangbridge/src/lsp_navigation.erl
@@ -15,7 +15,8 @@ internal_goto_definition(File, Line, Column) ->
         undefined ->
             #{result => <<"ko">>};
         _ ->
-            What = element_at_position(FileSyntaxTree, Line, Column),
+            Module = list_to_atom(filename:basename(File)),
+            What = element_at_position(Module, FileSyntaxTree, Line, Column),
             case find_element(What, FileSyntaxTree, File) of
                 {FoundFile, FoundLine, FoundColumn} ->
                     #{result => <<"ok">>, uri => list_to_binary("file://" ++ FoundFile), line => FoundLine, character => FoundColumn};
@@ -51,18 +52,36 @@ find_in_file_syntax_tree(FileSyntaxTree, Fun) ->
         end, Acc, TopLevelSyntaxTree)
     end, undefined, FileSyntaxTree).
 
-element_at_position(FileSyntaxTree, Line, Column) ->
+element_at_position(CurrentModule, FileSyntaxTree, Line, Column) ->
     Fun = fun
         ({call, {L, StartColumn}, {atom, {L, StartColumn}, Function}, Args}) when L =:= Line andalso StartColumn =< Column ->
             EndColumn = StartColumn + length(atom_to_list(Function)),
             if
-                Column =< EndColumn -> {local_call, Function, length(Args)};
+                Column =< EndColumn -> {function_use, CurrentModule, Function, length(Args)};
                 true -> undefined
             end;
-        ({call, {_, _}, {remote, {_, _}, {atom, {L, StartColumn}, Module}, {atom, {_, _}, Function}}, Args}) when L =:= Line andalso StartColumn =< Column ->
+        ({call, {_, _}, {remote, {_, _}, {atom, {_, _}, Module}, {atom, {L, StartColumn}, Function}}, Args}) when L =:= Line andalso StartColumn =< Column ->
             EndColumn = StartColumn + length(atom_to_list(Module)) + 1 + length(atom_to_list(Function)),
             if
-                Column =< EndColumn -> {remote_call, Module, Function, length(Args)};
+                Column =< EndColumn -> {function_use, Module, Function, length(Args)};
+                true -> undefined
+            end;
+        ({'fun',{L, StartColumn}, {function, Function, Arity}}) when L =:= Line andalso StartColumn =< Column ->
+            EndColumn = StartColumn + 4 + length(atom_to_list(Function)),
+            if
+                Column =< EndColumn -> {function_use, CurrentModule, Function, Arity};
+                true -> undefined
+            end;
+        ({'fun', {_, _}, {function, {atom, {_, _}, Module}, {atom, {L, StartColumn}, Function}, {integer, {_, _}, Arity}}}) when L =:= Line andalso StartColumn =< Column ->
+            EndColumn = StartColumn + length(atom_to_list(Function)),
+            if
+                Column =< EndColumn -> {function_use, Module, Function, Arity};
+                true -> undefined
+            end;
+        ({var, {L, StartColumn}, Variable}) when L =:= Line andalso StartColumn =< Column ->
+            EndColumn = StartColumn + length(atom_to_list(Variable)),
+            if
+                Column =< EndColumn -> {variable, Variable, L, StartColumn};
                 true -> undefined
             end;
         (_SyntaxTree) ->
@@ -70,18 +89,57 @@ element_at_position(FileSyntaxTree, Line, Column) ->
     end,
     find_in_file_syntax_tree(FileSyntaxTree, Fun).
 
-find_element({local_call, Function, Arity}, FileSyntaxTree, File) ->
-    case find_function(FileSyntaxTree, Function, Arity) of
+find_element({function_use, Module, Function, Arity}, CurrentFileSyntaxTree, CurrentFile) ->
+    CurrentModule = list_to_atom(filename:basename(CurrentFile)),
+    {File, SyntaxTree} = case Module of
+        CurrentModule ->
+            {CurrentFile, CurrentFileSyntaxTree};
+        _ ->
+            OtherFile = filename:dirname(CurrentFile) ++ "/" ++ atom_to_list(Module) ++ ".erl",
+            {OtherFile, file_syntax_tree(OtherFile)} % TODO search in project not in the file directory
+    end,
+    case find_function(SyntaxTree, Function, Arity) of
         {Line, Column} -> {File, Line, Column};
         _ -> undefined
     end;
-find_element({remote_call, Module, Function, Arity}, _FileSyntaxTree, File) ->
-    OtherFile = filename:dirname(File) ++ "/" ++ atom_to_list(Module) ++ ".erl",
-    OtherFileSyntaxTree = file_syntax_tree(OtherFile), % TODO search in project not in the file directory
-    case find_function(OtherFileSyntaxTree, Function, Arity) of
-        {Line, Column} -> {OtherFile, Line, Column};
-        _ -> undefined
+find_element({variable, Variable, Line, Column}, CurrentFileSyntaxTree, CurrentFile) ->
+    AllClauses = lists:foldl(fun (TopLevelSyntaxTree, Acc) ->
+        erl_syntax_lib:fold(fun (SyntaxTree, SingleAcc) ->
+            case SyntaxTree of
+                {clause, {_, _}, _, _, _} ->
+                    [SyntaxTree | SingleAcc];
+                _ ->
+                    SingleAcc
+            end
+        end, Acc, TopLevelSyntaxTree)
+    end, [], CurrentFileSyntaxTree),
+    ClausesWithVariable = lists:keysort(2, lists:filter(fun (ClauseSyntaxTree) ->
+        lists:member({Line, Column}, find_variable_in_tree(Variable, ClauseSyntaxTree))
+    end, AllClauses)),
+    case ClausesWithVariable of
+        [_H | _T] ->
+            Occurrences = find_variable_in_tree(Variable, lists:last(ClausesWithVariable)),
+            case Occurrences of
+                [{L, C} | _Tail] ->
+                    {CurrentFile, L, C};
+                _ ->
+                    undefined
+            end;
+        _ ->
+            undefined
     end.
+
+find_variable_in_tree(Variable, Tree) ->
+    lists:sort(find_variable_in_tree(Variable, Tree, [])).
+
+find_variable_in_tree(Variable, {var, {Line, Column}, Variable}, Acc) ->
+    [{Line, Column} | Acc];
+find_variable_in_tree(Variable, T, Acc) when is_tuple(T) ->
+    find_variable_in_tree(Variable, tuple_to_list(T), Acc);
+find_variable_in_tree(Variable, [H | T], Acc) ->
+     find_variable_in_tree(Variable, T, find_variable_in_tree(Variable, H, Acc));
+find_variable_in_tree(_Variable, _, Acc) ->
+    Acc.
 
 find_function(FileSyntaxTree, Function, Arity) ->
     Fun = fun
