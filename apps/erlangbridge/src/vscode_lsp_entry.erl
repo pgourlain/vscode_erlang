9666bb9322f3023822b22a8d6d98d26296aae242
Wojtek Surowka <wojteksurowka@me.com>
2018-05-27 22:19:30 +0100

    lsp_utils added

--- apps/erlangbridge/src/vscode_lsp_entry.erl
+++ apps/erlangbridge/src/vscode_lsp_entry.erl
@@ -29,7 +29,7 @@ compile_needed_modules() ->
     CompileOptions = [verbose, binary, report],
     do_compile(["src/vscode_lsp_app", "src/gen_lsp_server", 
         "src/gen_lsp_sup", "src/gen_lsp_doc_sup","src/gen_lsp_doc_server",
-        "src/gen_lsp_help_sup","src/gen_lsp_help_server", "src/lsp_handlers",
+        "src/gen_lsp_help_sup","src/gen_lsp_help_server", "src/lsp_handlers", "src/lsp_utils",
         "src/vscode_lsp_app_sup", "src/lsp_navigation", "src/lsp_syntax", "src/lsp_completion",
         "src/gen_connection", "src/vscode_jsone","src/vscode_jsone_decode","src/hover_doc_layout"], CompileOptions)
     .
