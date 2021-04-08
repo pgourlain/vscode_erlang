585bb46796a50edac074585fd6004a7f4375827f
shuying <15079462244@163.com>
2021-04-09 02:06:48 +0800

    fix #146 fix #120 fix #189

--- apps/erlangbridge/src/lsp_navigation.erl
+++ apps/erlangbridge/src/lsp_navigation.erl
@@ -293,8 +293,16 @@ element_at_position(CurrentModule, FileSyntaxTree, Line, Column, LineContents) -
                 Column =< EndColumn -> {function_use, CurrentModule, Function, length(Args)};
                 true -> undefined
             end;
-        ({attribute, {L, StartColumn}, export, _Exports}, _) when L =:= Line andalso StartColumn =< Column ->
-            export;
+        ({attribute, {_L, _StartColumn}, export, _Exports}, _) ->
+            find_function_in_export(CurrentModule, LineContents, Column);
+        ({attribute, _, file, {HrlFile, _}}, _) ->
+            BaseName = filename:basename(HrlFile),
+            Case1 = list_to_binary("-include(\"" ++ BaseName ++ "\").\r") == LineContents andalso Column > length("-include(") andalso Column =< length("-include(") + length(BaseName),
+            Case2 = list_to_binary("-include_lib(\"" ++ BaseName ++ "\").\r") == LineContents andalso Column > length("-include_lib(") andalso Column < length("-include_lib(") + length(BaseName),
+            if 
+                Case1; Case2 -> {hrl, HrlFile};
+                true -> undefined
+            end;
         ({call, {_, _}, {remote, {_, _}, {atom, {_, MStartColumn}, Module}, {atom, {L, StartColumn}, Function}}, Args}, _) when L =:= Line andalso MStartColumn =< Column ->
             MEndColumn = MStartColumn + length(atom_to_list(Module)), 
             EndColumn = StartColumn + length(atom_to_list(Module)) + 1 + length(atom_to_list(Function)),
@@ -335,7 +343,7 @@ element_at_position(CurrentModule, FileSyntaxTree, Line, Column, LineContents) -
                 Column =< EndColumn -> {record, Record};
                 true -> find_record_field_use(Record, Fields, Column)
             end;
-        ({record_field, {L, RecordSttartColumn}, _, Record, {atom, {L, FieldStartColumn}, Field}}, _) when L =:= Line andalso RecordSttartColumn =< Column ->
+        ({record_index, {L, RecordSttartColumn}, Record, {atom, {L, FieldStartColumn}, Field}}, _) when L =:= Line andalso RecordSttartColumn =< Column ->
             FieldEndColumn = FieldStartColumn + length(atom_to_list(Field)),
             if
                 Column < FieldStartColumn -> {record, Record};
@@ -348,7 +356,7 @@ element_at_position(CurrentModule, FileSyntaxTree, Line, Column, LineContents) -
     case find_macro_use(LineContents, Column) of
         undefined ->
             case find_in_file_syntax_tree(FileSyntaxTree, Fun) of
-                {export, _File} -> find_function_in_export(CurrentModule, LineContents, Column);
+                {{function_use, _, _, _} = Export, _File} -> Export;
                 {What, _File} -> What
             end;
         MacroUse -> MacroUse
@@ -399,6 +407,8 @@ find_element({module_use, Module}, _CurrentFileSyntaxTree, _CurrentFile) ->
         {attribute, {Line, Column}, _} -> {File, Line, Column};
         _ -> undefined
     end;
+find_element({hrl, HrlFile}, _CurrentFileSyntaxTree, _CurrentFile) ->
+    {HrlFile, 1, 1};
 find_element({function_use, Module, Function, Arity}, _CurrentFileSyntaxTree, _CurrentFile) ->
     {SyntaxTree, File} = lsp_syntax:module_syntax_tree(Module),
     case find_function(SyntaxTree, Function, Arity) of
@@ -530,6 +540,8 @@ find_record(FileSyntaxTree, Record) ->
 find_record_field(_Field, [], _CurrentFile) ->
     undefined;
 find_record_field(Field, [{record_field, _, {atom, {Line, Column}, Field}} | _], CurrentFile) ->
+    {CurrentFile, Line, Column};
+find_record_field(Field, [{record_field, _, {atom, {Line, Column}, Field}, _} | _], CurrentFile) ->
     {CurrentFile, Line, Column};
 find_record_field(Field, [_ | Tail], CurrentFile) ->
     find_record_field(Field, Tail, CurrentFile).
