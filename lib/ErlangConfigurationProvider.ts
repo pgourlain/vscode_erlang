009220cbb3308442fe0bc041a10b8006d66510bc
Pierrick Gourlain <pgourlain@hotmail.com>
2023-05-07 21:47:08 +0200

    remove deprecated 'rootpath' (#258)
    * remove deprecated rootPath

--- lib/ErlangConfigurationProvider.ts
+++ lib/ErlangConfigurationProvider.ts
@@ -2,6 +2,7 @@ import {
 	workspace, WorkspaceFolder, DebugConfiguration, DebugConfigurationProvider, CancellationToken, ProviderResult
 } from 'vscode'; 
 import { ErlangSettings } from './erlangSettings';
+//import { ErlangOutput } from './vscodeAdapter';
 
 export class ErlangDebugConfigurationProvider implements DebugConfigurationProvider {
     provideDebugConfigurations?(folder: WorkspaceFolder | undefined, token?: CancellationToken): ProviderResult<DebugConfiguration[]> {
@@ -31,15 +32,29 @@ export function configurationChanged() : void {
         includePaths: erlangConf.get("includePaths", []),
         linting: erlangConf.get<boolean>('linting', false),
         rebarBuildArgs: erlangConf.get("rebarBuildArgs", ['compile']),
-        rootPath : workspace.rootPath,
+        rootPath : extractRootPath(),
         verbose: erlangConf.get("verbose", false)
     };
     currentSettings = settings;
 }
 
+function extractRootPath() : string {
+    //workspace.rootPath is deprecated, and documentation notice that value is store in first workspace folder
+    let folders = workspace.workspaceFolders;
+    if (folders && folders.length > 0) {
+        return folders[0].uri.fsPath;
+    }
+    return undefined;
+}
+
 export function getElangConfigConfiguration() : ErlangSettings {
     if (!currentSettings) {
         configurationChanged();
+        // in order to debug
+        // let output = ErlangOutput();
+        // output.appendLine("workspace information");
+        // output.appendLine(`rootPath: ${workspace.rootPath}`);
+        // output.appendLine(`workspaceFolders: ${JSON.stringify(workspace.workspaceFolders)}`);
     }
     return currentSettings;
 }
